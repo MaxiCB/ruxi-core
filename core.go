97dca3857de9fc2ebc65df7f1c5a7d8041b55663
Aaron Beasley <aaroncb@aaroncb.com>
2022-06-09 21:11:50 -0500

    feat(db): ADD InitDB

--- core.go
+++ core.go
@@ -1,5 +1,34 @@
 package ruxicore
 
-func TestExample() string {
-	return "RUXI CORE TESTING"
+import (
+	"context"
+	"database/sql"
+	"os"
+
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/driver/pgdriver"
+)
+
+type DB struct {
+	connection *pgdriver.Connector
+	sqlDB      *sql.DB
+	bunDB      *bun.DB
+	context    context.Context
+}
+
+func InitDB(app_name string) DB {
+	db := DB{}
+	db.connection = pgdriver.NewConnector(
+		pgdriver.WithAddr(os.Getenv("DB_HOST")+":"+os.Getenv("DB_PORT")),
+		pgdriver.WithUser(os.Getenv("DB_USERNAME")),
+		pgdriver.WithPassword(os.Getenv("DB_PASSWORD")),
+		pgdriver.WithDatabase(os.Getenv("DB")),
+		pgdriver.WithApplicationName(app_name),
+		pgdriver.WithInsecure(true),
+	)
+	db.sqlDB = sql.OpenDB(db.connection)
+	db.bunDB = bun.NewDB(db.sqlDB, pgdialect.New())
+	db.context = context.Background()
+	return db
 }
