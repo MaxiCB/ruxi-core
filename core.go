39533097939305a53f89c2dbea424cf8f77bf569
Aaron Beasley <aaroncb@aaroncb.com>
2022-06-09 21:30:51 -0500

    feat(core): FIX VerifySession

--- core.go
+++ core.go
@@ -15,15 +15,15 @@ import (
 )
 
 type DB struct {
-	connection *pgdriver.Connector
-	sqlDB      *sql.DB
-	bunDB      *bun.DB
-	context    context.Context
+	Connection *pgdriver.Connector
+	SqlDB      *sql.DB
+	BunDB      *bun.DB
+	Context    context.Context
 }
 
-func InitDB(app_name string) *DB {
+func InitDB(app_name string) DB {
 	db := DB{}
-	db.connection = pgdriver.NewConnector(
+	db.Connection = pgdriver.NewConnector(
 		pgdriver.WithAddr(os.Getenv("DB_HOST")+":"+os.Getenv("DB_PORT")),
 		pgdriver.WithUser(os.Getenv("DB_USERNAME")),
 		pgdriver.WithPassword(os.Getenv("DB_PASSWORD")),
@@ -31,13 +31,13 @@ func InitDB(app_name string) *DB {
 		pgdriver.WithApplicationName(app_name),
 		pgdriver.WithInsecure(true),
 	)
-	db.sqlDB = sql.OpenDB(db.connection)
-	db.bunDB = bun.NewDB(db.sqlDB, pgdialect.New())
-	db.context = context.Background()
-	return &db
+	db.SqlDB = sql.OpenDB(db.Connection)
+	db.BunDB = bun.NewDB(db.SqlDB, pgdialect.New())
+	db.Context = context.Background()
+	return db
 }
 
-func verifySession(options *sessmodels.VerifySessionOptions) gin.HandlerFunc {
+func VerifySession(options *sessmodels.VerifySessionOptions) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		session.VerifySession(options, func(rw http.ResponseWriter, r *http.Request) {
 			c.Request = c.Request.WithContext(r.Context())
