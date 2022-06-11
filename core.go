ce799e607db4a5fa1184bb5f7ffad9a7cd228078
MaxiCB <aaroncb@aaroncb.com>
2022-06-11 18:07:05 -0500

    feat(ruxi-core): ADD Ruxi-Core Utils
    - ADD HealthCheck
    - ADD RuxiLogger
    - ADD RuxiGin

--- core.go
+++ core.go
@@ -3,22 +3,28 @@ package ruxicore
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/supertokens/supertokens-golang/recipe/session"
 	"github.com/supertokens/supertokens-golang/recipe/session/sessmodels"
+	"github.com/supertokens/supertokens-golang/supertokens"
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
 	"github.com/uptrace/bun/driver/pgdriver"
+	"github.com/uptrace/bun/extra/bundebug"
 )
 
 type DB struct {
 	connection *pgdriver.Connector
-	sqlDB      *sql.DB
-	bunDB      *bun.DB
-	context    context.Context
+	SqlDB      *sql.DB
+	BunDB      *bun.DB
+	Context    context.Context
 }
 
 func InitDB(app_name string) *DB {
@@ -31,9 +37,15 @@ func InitDB(app_name string) *DB {
 		pgdriver.WithApplicationName(app_name),
 		pgdriver.WithInsecure(true),
 	)
-	db.sqlDB = sql.OpenDB(db.connection)
-	db.bunDB = bun.NewDB(db.sqlDB, pgdialect.New())
-	db.context = context.Background()
+	db.SqlDB = sql.OpenDB(db.connection)
+	db.BunDB = bun.NewDB(db.SqlDB, pgdialect.New())
+	db.Context = context.Background()
+	if os.Getenv("DB_LOGS") != "" {
+		db.BunDB.AddQueryHook(bundebug.NewQueryHook(bundebug.WithVerbose(true)))
+	}
+	if dbErr := db.BunDB.Ping(); dbErr != nil {
+		panic(dbErr)
+	}
 	return &db
 }
 
@@ -45,4 +57,49 @@ func VerifySession(options *sessmodels.VerifySessionOptions) gin.HandlerFunc {
 		})(c.Writer, c.Request)
 		c.Abort()
 	}
+}
+
+func HealthCheck(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"status": "UP"})
+}
+
+func RuxiLogger() gin.HandlerFunc {
+	gin.DisableConsoleColor()
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+	return gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	})
+}
+
+func RuxiGin() *gin.Engine {
+	router := gin.Default()
+	router.Use(RuxiLogger())
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "DELETE", "PUT", "OPTIONS"},
+		AllowHeaders: append([]string{"content-type"},
+			supertokens.GetAllCORSHeaders()...),
+		AllowCredentials: true,
+	}))
+
+	router.Use(func(c *gin.Context) {
+		supertokens.Middleware(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+			c.Next()
+		})).ServeHTTP(c.Writer, c.Request)
+		c.Abort()
+	})
+
+	router.GET("/liveness", HealthCheck)
+	return router
 }
--- go.mod
+++ go.mod
@@ -3,14 +3,17 @@ module github.com/MaxiCB/ruxi-core
 go 1.18
 
 require (
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.8.1
 	github.com/supertokens/supertokens-golang v0.6.6
 	github.com/uptrace/bun v1.1.5
 	github.com/uptrace/bun/dialect/pgdialect v1.1.5
 	github.com/uptrace/bun/driver/pgdriver v1.1.5
+	github.com/uptrace/bun/extra/bundebug v1.1.5
 )
 
 require (
+	github.com/fatih/color v1.13.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
@@ -21,6 +24,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
